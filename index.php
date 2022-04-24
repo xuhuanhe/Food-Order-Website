281cf5aadc8bcfb5caac53616c42d2ebccdc51e6
xuhuan he <xuhuanhe16@gmail.com>
2022-04-23 21:08:03 -0400

    Displaying Categories on Home Page

--- index.php
+++ index.php
@@ -17,30 +17,49 @@
     <section class="categories">
         <div class="container">
             <h2 class="text-center">Explore Foods</h2>
+            <?php
+                //create sql query to display categories from database
+                $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' LIMIT 2";
+                //execute the query
+                $res = mysqli_query($conn, $sql);
+                //count rows to check whether the category is available or not
+                $count = mysqli_num_rows($res);
 
-            <a href="category-foods.html">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+                if($count > 0){
+                    //categories available
+                    while($row=mysqli_fetch_assoc($res)){
+                        //get the values like title, image_name
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $image_name = $row['image_name'];
+                        ?>
+                        <a href="category-foods.html">
+                            <div class="box-3 float-container">
+                                <?php 
+                            //check whether image is available or not
+                                    if($image_name==""){
+                                        //display message
+                                        echo "<div class ='error'>image not available</div>";
+                                    }else{
+                                        //image available
+                                        ?>
+                                        <img src="<?php echo SITEURL; ?>/images/category/<?php echo $image_name?>" alt="Pizza" class="img-responsive img-curve">
+                                        <?php
+                                    }
+                                ?>
+                                
 
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Burger</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Momo</h3>
-            </div>
-            </a>
+                                <h3 class="float-text text-white"><?php echo $title; ?></h3>
+                            </div>
+                        </a>
+                        
+                        <?php
+                    }
+                }else{
+                    //categories not available
+                    echo "<div class='error'>Category not added. </div>";
+                }
+            ?>
 
             <div class="clearfix"></div>
         </div>
@@ -167,4 +186,4 @@
     </section>
     <!-- fOOD Menu Section Ends Here -->
 
-    <?php include('partials-front/footer.php')?>
+    <?php include('partials-front/menu.php')?>
