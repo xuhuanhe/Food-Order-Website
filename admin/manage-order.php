90a93fbcdf8a3c9b01ad83c6d71dea8db24be56c
xuhuan he <xuhuanhe16@gmail.com>
2022-04-30 12:00:02 -0400

    Managing food from admin panel

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -10,37 +10,69 @@
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full time</th>
-                        <th>Username</th>
-                        <th>Action</th>
+                        <th>Food</th>
+                        <th>Price</th>
+                        <th>Qty</th>
+                        <th>Total</th>
+                        <th>Order Date</th>
+                        <th>Status</th>
+                        <th>Customer Name</th>
+                        <th>Customer contact</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Actions</th>
                     </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>Xuhuan He</td>
-                        <td>xuhuanhe</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>Xuhuan He</td>
-                        <td>xuhuanhe</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>Xuhuan He</td>
-                        <td>xuhuanhe</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+
+                    <?php
+                        //get all the orders from database
+                        $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; //display the latest order as first
+                        //execute the query
+                        $res = mysqli_query($conn, $sql);
+                        //count the rows
+                        $count = mysqli_num_rows($res);
+
+                        $sn = 1;//create a serial number and set its initail values as 1
+                        if($count > 0){
+                            //order available
+                            while($row=mysqli_fetch_assoc($res)){
+                                //get all the order details
+                                $id = $row['id'];
+                                $food = $row['food'];
+                                $price = $row['price'];
+                                $qty = $row['qty'];
+                                $total = $row['total'];
+                                $order_date = $row['order_date'];
+                                $status = $row['status'];
+                                $customer_name = $row['customer_name'];
+                                $customer_contact = $row['customer_name'];
+                                $customer_email = $row['customer_email'];
+                                $customer_address = $row['customer_address'];
+
+                                ?>
+                                    <tr>
+                                        <td><?php echo $sn++; ?></td>
+                                        <td><?php echo $food; ?></td>
+                                        <td><?php echo $price; ?></td> 
+                                        <td><?php echo $qty; ?></td>
+                                        <td><?php echo $total; ?></td>
+                                        <td><?php echo $order_date; ?></td>
+                                        <td><?php echo $status; ?></td>
+                                        <td><?php echo $customer_name; ?></td>
+                                        <td><?php echo $customer_contact; ?></td>
+                                        <td><?php echo $customer_email; ?></td>
+                                        <td><?php echo $customer_address; ?></td>
+                                        <td>
+                                            <a href="#" class="btn-secondary">Update Order</a>
+                                        </td>
+                                    </tr>
+                                <?php
+                            }
+                        }else{
+                            //order not available
+                            echo "<tr><td colspan='12' class='error'>Orders not available</td></tr>";
+                        }
+                    ?>
+
                 </table>
 
 
