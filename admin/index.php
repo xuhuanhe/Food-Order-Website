637c82edf9e72c461de3b35d6d886166eccac73c
Xuhuan He <xuhuanhe16@example.com>
2022-05-15 19:21:57 -0400

    Created Summary Dashboard on Admin Panel

--- admin/index.php
+++ admin/index.php
@@ -14,27 +14,49 @@
                 ?>
                 <br><br>
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql = "SELECT * FROM tbl_category";
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
+                    ?>
+                    <h1><?php echo $count ?></h1>
                     <br>
                     Categories
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql2 = "SELECT * FROM tbl_food";
+                        $res2 = mysqli_query($conn, $sql2);
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+                    <h1><?php echo $count2 ?></h1>
                     <br>
-                    Categories
+                    Foods
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql3 = "SELECT * FROM tbl_order";
+                        $res3 = mysqli_query($conn, $sql3);
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+                    <h1><?php echo $count3 ?></h1>
                     <br>
-                    Categories
+                    Total Orders
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql4 = "SELECT SUM(total) AS total FROM tbl_order WHERE status='Delivered'";
+                        $res4 = mysqli_query($conn, $sql4);
+                        $row4 = mysqli_fetch_assoc($res4);
+                        //get the total revenue
+                        $total_revenue = $row4['total'];
+                    ?>
+                    <h1><h1><?php echo $total_revenue ?></h1></h1>
                     <br>
-                    Categories
+                    Revenue Generated
                 </div>
                 <div class="clearfix"></div>
             </div>
